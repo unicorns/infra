0b793e7f8d14c105798c780fbbd6a520a3c93b31
Ben Zhang <ben.zhang@watonomous.ca>
2024-05-15 06:48:56 +0000

    Import the infra repo

--- github/main.tf
+++ github/main.tf
@@ -39,8 +39,26 @@ provider "github" {
   }
 }
 
-data github_repository "infra" {
+resource github_repository infra {
   name = "infra"
+  description = "Infrastructure configuration for the 🦄"
+  has_issues = true
+  has_projects = true
+  vulnerability_alerts = true
+
+  allow_merge_commit = false
+  allow_squash_merge = true
+  squash_merge_commit_title = "PR_TITLE"
+  squash_merge_commit_message = "PR_BODY"
+  allow_auto_merge = true
+  delete_branch_on_merge = true
+
+  # Safety feature to prevent accidental deletion
+  archive_on_destroy = true
+}
+import {
+  id = "infra"
+  to = github_repository.infra
 }
 
 resource tls_private_key infra-deploy-key {
@@ -49,14 +67,14 @@ resource tls_private_key infra-deploy-key {
 }
 
 resource github_repository_deploy_key "infra" {
-  repository = data.github_repository.infra.name
+  repository = github_repository.infra.name
   title = "Terraform-managed deploy key"
   key = tls_private_key.infra-deploy-key.public_key_openssh
   read_only = false
 }
 
 resource github_actions_secret "infra-deploy-key" {
-  repository = data.github_repository.infra.name
+  repository = github_repository.infra.name
   secret_name = "DEPLOY_KEY"
   plaintext_value = tls_private_key.infra-deploy-key.private_key_openssh
 }
