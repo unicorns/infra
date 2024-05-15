57b13fcf4bddb37bb14d9fb33578953d34d10e29
Ben Zhang <ben.zhang@watonomous.ca>
2024-05-15 08:24:37 +0000

    Standardize secret paths

--- kubernetes/tailscale/preprovision.py
+++ kubernetes/tailscale/preprovision.py
@@ -1,7 +1,10 @@
 from common.provisioner_utils import ProvisionerTools
+from common.variables import TAILSCALE_KUBERNETES_OPERATOR_SECRETS_PATH
 
 def get_vars(tools: ProvisionerTools, project: str):
-    tailscale_operator_secret = tools.vault_client.secrets.kv.v2.read_secret(path="tailscale/kubernetes-operator")["data"]["data"]
+    tailscale_operator_secret = tools.vault_client.secrets.kv.v2.read_secret(
+        TAILSCALE_KUBERNETES_OPERATOR_SECRETS_PATH
+    )["data"]["data"]
 
     return {
         'tailscale_operator_client_id': tailscale_operator_secret["oauth_client_id"],
