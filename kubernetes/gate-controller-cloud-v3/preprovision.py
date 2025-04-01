33c5d0734a703c966c435db2faf3dbcca623e094
Ben Zhang <benzhangniu@gmail.com>
2025-04-01 13:30:48 -0700

    Delete gcc3 from sweden kubernetes

--- kubernetes/gate-controller-cloud-v3/preprovision.py
+++
@@ -1,9 +0,0 @@
-from common.provisioner_utils import ProvisionerTools
-from common.variables import GATE_CONTROLLER_CLOUD_V3_ADDR
-from urllib.parse import urlparse
-
-def get_vars(tools: ProvisionerTools, project: str):
-    return {
-        'host': urlparse(GATE_CONTROLLER_CLOUD_V3_ADDR).hostname,
-        'initial_admin_credentials': tools.vault_client.secrets.kv.v2.read_secret(path=f"kubernetes/{project}")["data"]["data"]["initial_admin_credentials"]
-    }
