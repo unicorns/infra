7643834e405a739821b7fe7230e072749ee3a479
Ben Zhang <benzhangniu@gmail.com>
2025-04-01 13:17:58 -0700

    Delete elastic

--- kubernetes-aks2/elastic/preprovision.py
+++
@@ -1,9 +0,0 @@
-from common.provisioner_utils import ProvisionerTools
-from common.variables import KIBANA_ADDR, ELASTICSEARCH_ADDR
-from urllib.parse import urlparse
-
-def get_vars(tools: ProvisionerTools, project: str):
-    return {
-        "elasticsearch_host": urlparse(ELASTICSEARCH_ADDR).hostname,
-        "kibana_host": urlparse(KIBANA_ADDR).hostname,
-    }
