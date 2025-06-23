2da9100a622c013572c81f70e085e88e4b51cde7
Ben Zhang <5977478+ben-z@users.noreply.github.com>
2025-06-23 09:26:10 -0700

    Delete Vault from AKS1 (#73)
    In #72 we deployed Vault on AKS2. This PR deletes vault from AKS1.

--- kubernetes/vault/preprovision.py
+++
@@ -1,8 +0,0 @@
-from common.provisioner_utils import ProvisionerTools
-from common.variables import HASHICORP_VAULT_ADDR
-from urllib.parse import urlparse
-
-def get_vars(tools: ProvisionerTools, project: str):
-    return {
-        'vault_host': urlparse(HASHICORP_VAULT_ADDR).hostname,
-    }
