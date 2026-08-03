9a889909c652a0ef047fe241d48822dcf9b926b4
Ben Zhang <benzhangniu@gmail.com>
2026-08-02 17:13:09 -0700

    codex: remove legacy Vault dependency from CI (#74)

--- common/workflow_utils.py
+++ common/workflow_utils.py
@@ -7,13 +7,38 @@ import json
 
 from common.cli_utils import get_app
 
-PROVISIONERS = sorted([
-    p.parent.name for p in Path(__file__).parent.parent.glob("*/provision.py")
-    # Skip some provisioners in CI
-    if p.parent.name not in [
-        "vault", # requires elevated permissions
-    ]
-])
+PROVISIONERS = {
+    "azure": {
+        "env": {
+            "TF_TOKEN_app_terraform_io": "${{ secrets.TF_TOKEN_APP_TERRAFORM_IO }}",
+            "ARM_SUBSCRIPTION_ID": "${{ secrets.ARM_SUBSCRIPTION_ID }}",
+            "ARM_CLIENT_ID": "${{ secrets.ARM_CLIENT_ID }}",
+            "ARM_TENANT_ID": "${{ secrets.ARM_TENANT_ID }}",
+            "ARM_CLIENT_SECRET": "${{ secrets.ARM_CLIENT_SECRET }}",
+            "AZURE_KEY_VAULT_ADMIN_OBJECT_IDS": "${{ vars.AZURE_KEY_VAULT_ADMIN_OBJECT_IDS }}",
+        },
+    },
+    "cloudflare": {
+        "dependencies": ["Provision azure"],
+        "env": {
+            "TF_TOKEN_app_terraform_io": "${{ secrets.TF_TOKEN_APP_TERRAFORM_IO }}",
+            "CLOUDFLARE_API_TOKEN": "${{ secrets.CLOUDFLARE_API_TOKEN }}",
+            "GATE_CONTROLLER_ORIGIN_IP": "${{ vars.INGRESS_EXTERNAL_IP }}",
+        },
+    },
+    "kubernetes-shared": {
+        "dependencies": ["Provision azure"],
+        "env": {
+            "TF_TOKEN_app_terraform_io": "${{ secrets.TF_TOKEN_APP_TERRAFORM_IO }}",
+            "KUBE_CONFIG": "${{ secrets.KUBE_CONFIG }}",
+            "AKS_CLUSTER_NAME": "${{ vars.AKS_CLUSTER_NAME }}",
+            "AZURE_KEY_VAULT_NAME": "${{ vars.AZURE_KEY_VAULT_NAME }}",
+            "AZURE_TENANT_ID": "${{ secrets.ARM_TENANT_ID }}",
+            "KEY_VAULT_SECRET_PROVIDER_CLIENT_ID": "${{ vars.KEY_VAULT_SECRET_PROVIDER_CLIENT_ID }}",
+            "INGRESS_EXTERNAL_IP": "${{ vars.INGRESS_EXTERNAL_IP }}",
+        },
+    },
+}
 
 app = get_app()
 
@@ -27,8 +52,9 @@ def generate_provision_workflow(jsonnet_file: str):
                 {
                     "name": f"Provision {p}",
                     "command": f"docker compose run provisioner ./{p}/provision.py all",
-                    "requires_vault": True,
-                } for p in PROVISIONERS
+                    **options,
+                }
+                for p, options in PROVISIONERS.items()
             ])
         }
     )
